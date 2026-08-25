b1d81690ede6c7d41693ce298f751f006675744e
Hrutav Modha <modhahrutav@gmail.com>
2026-08-25 18:14:13 +0530

    Add support for computed state and fix few bugs

--- playground/src/drift.d.ts
+++ playground/src/drift.d.ts
@@ -4,3 +4,8 @@ declare module '*.drift' {
   const component: CompiledModule;
   export default component;
 }
+
+/**
+ * Declares a reactive computed / derived value in DriftJS Single File Components.
+ */
+declare function derive<T>(exprOrFn: T | (() => T)): T;
